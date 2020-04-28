1391331d7756ecb8a08afbdf5e75c0920c6699d3
Juan Manuel Garcia <jmgarcia@irpaglide.com>
2020-04-28 08:39:35 -0300

    reduce disk size

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ resource "aws_launch_template" "main" {
   block_device_mappings {
     device_name = "/dev/sda1"
     ebs {
-      volume_size = 35
+      volume_size = 15
       volume_type = "io1"
-      iops = "400"
+      iops = "300"
     }
   }
 }
